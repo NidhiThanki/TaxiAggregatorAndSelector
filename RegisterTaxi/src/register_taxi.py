818ce28d71cf2ec1a06ab558565c22eb994cf00c
NidhiThanki <103079279+NidhiThanki@users.noreply.github.com>
2022-04-15 14:59:48 -0700

    Delete RegisterTaxi

--- RegisterTaxi/src/register_taxi.py
+++
@@ -1,6 +0,0 @@
-from src.Services.register_taxi_services import Register_Taxi_Services
-
-taxi_services = Register_Taxi_Services()
-taxi_services.read_data_from_csv()
-taxi_services.insert_data_db()
-
