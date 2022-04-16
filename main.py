c1286499a595bdd8315c6ed0bfdfbd57a6dca305
mayuriph <mayuri.phanslkr@gmail.com>
2022-04-17 02:18:46 +0530

    updated customer code for: registering one/many customers, booking, getting customer details from mongodb,config files

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
 from src.Taxi.register_taxi_services import Register_Taxi_Services
+from src.Customer.customer_services import Customer_Services
 
 # This methods are used for get taxi data from CSV and pushing those into mongo db atlas
+print("###########################Taxi Services#################################")
 taxi_services = Register_Taxi_Services()
 taxi_services.read_data_from_csv()
 taxi_services.insert_data_db()
+
+
+print("###########################Customer Services#################################")
+# create customer object
+cust1 = Customer_Services()
+# register number of customers at once
+cust1.register_many()
+
+# # register one new customer
+# cust1.register_one("Mohan","Kumar","moh@gmail.com",13.950290,77.204570)
+
+# # Randomly booking for customers who are already registered for cab services
+# res=cust1.booking("Mohan","Kumar")
+# if res !=-1:
+#     print("=========Booking Successful !!===========")
+#     print("Taxi Details: ",res)
+# else:
+#     print("=========Booking Unsuccessful !!=========")
+
---
+++ src/Customer/customer_services.py
@@ -0,0 +1,154 @@
+
+# Imports ObjectId to convert to the correct format before querying in the db
+from asyncore import read
+from bson.objectid import ObjectId
+from random import randint
+import random,json
+import datetime
+import requests
+from src.Util.CommonUtil import CommonUtil
+
+# customer document contains customer_name (String), email (String), and role (String) fields
+class Customer_Services():
+    customer_data_list = []
+
+    def __init__(self):
+        self._latest_error = ''
+        config = CommonUtil.read_properties()
+        self._customer_file_path = config.get("CUSTOMER_CSV_FILE").data
+        self._collection_name = config.get("CUSTOMER_COLLECTION").data
+        self._max_lat = float(config.get("LAT_VALUE").data)
+        self._max_long = float(config.get("LONG_VALUE").data)
+        self.post_url = "https://dbco8t3hz3.execute-api.us-east-1.amazonaws.com/register-customer"
+        self.get_url = "https://dbco8t3hz3.execute-api.us-east-1.amazonaws.com/read-customer"
+        self.book_url = "https://pjfnpvj0ce.execute-api.us-east-1.amazonaws.com/customer-book"
+    
+    # Latest error is used to store the error string in case an issue. It's reset at the beginning of a new function call
+    @property
+    def latest_error(self):
+        return self._latest_error
+
+    # Reads customer.csv one line at a time, splits them into the data fields and inserts
+    def read_data_from_csv(self):
+        customer_data_list = []
+        with open(self._customer_file_path, 'r') as customer_fh:
+            for customer_row in customer_fh:
+                customer_row = customer_row.rstrip()
+                if customer_row:
+                    (customer_first_name, customer_last_name,customer_email,customer_trip_indicator) = customer_row.split(',')
+                            
+                customer_data = {"customer_first_name": customer_first_name,"customer_last_name": customer_last_name,"customer_email":customer_email,"customer_trip_indicator":customer_trip_indicator}
+                customer_data_list.append(customer_data)
+            return customer_data_list
+            
+    # generate uniques customer id 
+    def generate_customer_id(self,cust_name):
+        start = randint(1,10)
+        end = randint(11,20)
+        rand_num = randint(start,end)
+        cust_name = cust_name.replace(" ","")
+        cust_id = f"{cust_name}_{rand_num}"
+        return cust_id
+
+    # check for area boundary
+    def check_location(self,lat,long):
+        if self._max_lat <=  lat <= self._max_long  and  self._max_lat <=  lat <= self._max_long:
+            return 1
+        return -1
+        
+    # register one customer
+    def register_one(self,customer_first_name,customer_last_name,customer_email,lat,long):
+        try:
+            self._latest_error = '' 
+            # read customer customer details           
+            cust_res = self.get_customer_details(customer_first_name,customer_last_name)
+            if cust_res == -1:        
+                res = self.check_location(lat,long)                
+                if res == 1:            
+                    customer_id = self.generate_customer_id(customer_first_name)
+                    _timestamp=datetime.datetime.now()
+                    customer_data = {"timestamp":str(_timestamp),"customer_id":customer_id,"customer_first_name": customer_first_name,"customer_last_name": customer_last_name,"customer_email":customer_email,"customer_location":{"type":"Point","coordinates":[long,lat]},"customer_trip_indicator":"OFF"}        
+                    self.register_connection([customer_data])
+                    print("Successful Registration!")
+                else:
+                    print("Sorry,cab service is not available in this area!")
+            else:
+                print("Customer already exists!!")
+        except Exception as e:
+            print("RegisterOneError:",str(e))
+
+    # registers many customer from list/csv files
+    def register_many(self):
+        try:
+            customer_data = self.read_data_from_csv()
+            for val in customer_data:
+                customer_id = self.generate_customer_id(val['customer_first_name'])
+                _timestamp=datetime.datetime.now()
+                long = random.uniform(self._max_lat, self._max_long)
+                lat = random.uniform(self._max_lat, self._max_long)
+                val["timestamp"] = str(_timestamp)
+                val["customer_id"]=customer_id
+                val["customer_location"]={"type":"Point","coordinates":[long,lat]}
+                self.customer_data_list.append(val)
+            self.register_connection(self.customer_data_list)
+            print("Successful Registration!")
+        except Exception as e:
+            print("RegisterManyError:",str(e))        
+
+    # connect to API gateway for registration
+    def register_connection(self,cust_data):
+        #creaating json data
+        customer_data = json.dumps(cust_data)
+        # connecting to endpoint to send data
+        response = requests.post(self.post_url,data=customer_data)
+        if response.status_code == 200:
+            print("Connected to Register API Endpoint")
+        else:
+            print("Check status!")
+
+    # API endpoint to read customer details
+    def get_customer_details(self,customer_first_name,customer_last_name):
+        cust_data = {"customer_first_name":customer_first_name,"customer_last_name":customer_last_name}
+        # connecting to endpoint to send data
+        response = requests.get(self.get_url,params = cust_data)
+        if response.status_code == 200:
+            print("Connected to Read API Endpoint")
+            return response.text
+        else:
+            print(f"Check status:{response.text}")
+            return -1
+        
+
+    # raise booking request and return nearest taxi
+    def booking(self,customer_first_name,customer_last_name):
+        # read customer customer details           
+        res = self.get_customer_details(customer_first_name,customer_last_name)
+        if res != -1:
+            read_res = json.loads(res)
+            cust_loc= read_res["customer_location"]
+            lat = cust_loc["coordinates"][1]
+            long = cust_loc["coordinates"][0]
+            loc_res = self.check_location(lat,long)
+            if loc_res != -1 :
+                # connecting to endpoint to send data
+                cust_id = read_res["customer_id"]
+                cust_data = {"customer_id": cust_id,"lat":lat,"long":long,"type":"Point"}
+                response = requests.get(self.book_url,params = cust_data)
+                if response.status_code == 200:
+                    print("Connected to Booking API Endpoint")
+                    return response.text
+                else:
+                    print(f"Check status:{response.text}")
+                    return -1
+
+            
+
+                
+
+            
+            
+
+
+
+
+    
