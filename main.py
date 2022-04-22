9d89da891db3d9038a78451ce1e256ed0beae564
mayuriph <mayuri.phanslkr@gmail.com>
2022-04-22 09:30:09 +0530

    updated main.py

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ taxi_services = Taxi_Services()
 # This method will register taxies from csv file
 #taxi_services.register_taxi_from_csv()
 
-# This method will register single taxi
-taxi_services.register_single_taxi("KA05A7845", "Deluxe", "Toyota RAV4")
-print("Taxi Registered !!")
+# # This method will register single taxi
+# taxi_services.register_single_taxi("KA05A7845", "Deluxe", "Toyota RAV4")
+# print("Taxi Registered !!")
 
 print("###########################Customer Services#################################")
 # create customer object
@@ -22,6 +22,7 @@ cust = Customer_Services()
 
 # register one new customer via API
 cust1 = Customer_Services()
+#cust1.register_one("Mayuri","P","mayuri.phanslkr@gmail.com","Premium",12.73,77.56)
 #cust1.register_one("James","Valdez","JamesDez@gmail.com","General",12.73,77.56)
 
 
@@ -53,15 +54,14 @@ cust1 = Customer_Services()
 
 # # Randomly booking for customers who are already registered for cab services via API
 # res=cust1.booking(customer_first_name ="Captain",customer_last_name="A",source_lat=12.51,source_long=77.12,dest_lat=12.94,dest_long=77.43,taxi_type="Basic")
-
-res=cust1.booking(customer_first_name ="James",customer_last_name="Valdez",source_lat=12.60,source_long=77.20,dest_lat=12.94,dest_long=77.43,taxi_type="Any")
-if res == 0:
-    print("Customer not registered for services!")
-elif res !=-1:
-    print("Taxi Details: ",res)
-    # This method will start trip
-    taxi_services.start_trip(res)
-else:
-    print("=========Booking Unsuccessful !!=========")
+# book_res=cust1.booking(customer_first_name ="Mayuri",customer_last_name="P",source_lat=12.60,source_long=77.20,dest_lat=12.94,dest_long=77.43,taxi_type="All")
+# if book_res == 0:
+#     print("Customer not registered for services!")
+# elif book_res !=-1:
+#     print("Taxi Details: ",book_res)    
+# else:
+#     print("=========Booking Unsuccessful !!=========")
+# # This method will start trip
+#     taxi_services.start_trip(book_res)
 
 
